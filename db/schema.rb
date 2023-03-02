dd3a46ddb698251b21765252f28352a176a0dab2
MayakaOsinde <mayakaosinde@gmail.com>
2023-03-02 11:31:54 +0300

    Created new migrations to fix an error where the tables wouldnt display my user & tasks data.

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_075526) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_082607) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "due_date"
+    t.string "collaborators"
+    t.boolean "completed", default: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "middle_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "age"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "tasks", "users"
 end
