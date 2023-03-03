0411cbb2180fd7a423de8e9f6e99e9c989ecd52e
MayakaOsinde <mayakaosinde@gmail.com>
2023-03-03 19:02:45 +0300

    Updated the code in user controller to make user registration work: Fixed nomethod error bugs.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,54 +1,45 @@
+require 'bcrypt'
+require 'sinatra'
+require 'bcrypt'
+require 'json'
+require_relative '../models/user'
+
+
 class UserController < Sinatra::Base
 
-    set :default_content_type, 'application/json'
-
     # Lists all users
-    # get '/users' do
-    #     users = User.all
-    #     users.to_json
-    # end
+    get '/users' do
+        users = User.all
+        users.to_json
+    end
 
-    post '/auth/register' do
-        # Get user information from request body
-        data = JSON.parse(request.body.read)
-      
-        # Validate required fields
-        unless data['first_name'] && data['last_name'] && data['email'] && data['age'] && data['password']
-          halt 400, { error: 'Missing required fields' }.to_json
-        end
-      
-        # Validate email format
-        unless data['email'] =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-          halt 400, { error: 'Invalid email format' }.to_json
-        end
-      
-        # Validate age
-        unless data['age'].to_i > 0 && data['age'].to_i < 150
-          halt 400, { error: 'Invalid age' }.to_json
-        end
-      
-        # Hash the password using Bcrypt
-        password_hash = BCrypt::Password.create(data['password'])
-      
-        # Create a new user object with hashed password
-        user = {
-          first_name: data['first_name'],
-          middle_name: data['middle_name'],
-          last_name: data['last_name'],
-          email: data['email'],
-          age: data['age'],
-          password_hash: password_hash
-        }
-      
-        # Add user to the database
-        users[data['email']] = user
-      
+    post '/users/register' do
+      # Get user information from request body
+      data = JSON.parse(request.body.read)
+    
+      # Create a new user object
+      user = User.new(
+        first_name: data['first_name'],
+        middle_name: data['middle_name'],
+        last_name: data['last_name'],
+        email: data['email'],
+        age: data['age'],
+        password: data['password']
+      )
+    
+      # Save the user to the database
+      if user.save
         # Return success message
         { message: 'User created successfully' }.to_json
+      else
+        # Return error message
+        halt 400, { error: user.errors.full_messages.join(', ') }.to_json
+      end
     end
+    
       
       # Route for authenticating a user's credentials
-    post '/auth/login' do
+    post '/users/login' do
         # Get user information from request body
         data = JSON.parse(request.body.read)
       
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
+# class User < ActiveRecord::Base
+#     has_many :tasks
+#     validates :email, presence: true, uniqueness: true
+#     validates :password_digest, presence: true
+#     has_secure_password
+
+# end
+
+
 class User < ActiveRecord::Base
-    has_many :tasks
-    validates :email, presence: true, uniqueness: true
-    validates :password_digest, presence: true
-    has_secure_password
+  validates :first_name, :last_name, :email, :age, :password_digest, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+  validates :age, numericality: { greater_than: 0, less_than: 150 }
+
+  has_secure_password
 end
+
