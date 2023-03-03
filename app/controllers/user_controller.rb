6b96ecb0cafebb5d9662e0523a0e2e2493b2132e
MayakaOsinde <mayakaosinde@gmail.com>
2023-03-03 20:58:51 +0300

    Successfully completed user authentication in the User_controller method.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -39,25 +39,29 @@ class UserController < Sinatra::Base
     
       
       # Route for authenticating a user's credentials
-    post '/users/login' do
+      require 'sqlite3'
+
+      # Open the database
+      db = SQLite3::Database.open('your_database_name.db')
+      
+      post '/users/login' do
         # Get user information from request body
         data = JSON.parse(request.body.read)
-      
-        # Validate required fields
-        unless data['email'] && data['password']
-          halt 400, { error: 'Missing required fields' }.to_json
-        end
-      
+    
         # Find user by email
-        user = users[data['email']]
-      
-        # Check if user exists and password is correct
-        if user && BCrypt::Password.new(user[:password_hash]) == data['password']
-          # Return success message
-          { message: 'User authenticated successfully' }.to_json
+        user = User.find_by(email: data['email'])
+    
+        # Check if user exists and password matches
+        if user && user.authenticate(data['password'])
+          # Return success message with user details
+          { message: 'User authenticated successfully', user: user }.to_json
         else
           # Return error message
-          { error: 'Invalid email or password' }.to_json
+          halt 401, { error: 'Invalid email or password' }.to_json
         end
-    end
+      end
+        
+      # Close the database
+      # db.close
+      
 end
