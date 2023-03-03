602069bed63b4dc57a63360fccba42f7e5930bbd
MayakaOsinde <mayakaosinde@gmail.com>
2023-03-03 21:01:15 +0300

    Modulated my code.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -38,30 +38,24 @@ class UserController < Sinatra::Base
     end
     
       
-      # Route for authenticating a user's credentials
-      require 'sqlite3'
 
-      # Open the database
-      db = SQLite3::Database.open('your_database_name.db')
       
-      post '/users/login' do
-        # Get user information from request body
-        data = JSON.parse(request.body.read)
-    
-        # Find user by email
-        user = User.find_by(email: data['email'])
-    
-        # Check if user exists and password matches
-        if user && user.authenticate(data['password'])
-          # Return success message with user details
-          { message: 'User authenticated successfully', user: user }.to_json
-        else
-          # Return error message
-          halt 401, { error: 'Invalid email or password' }.to_json
-        end
+    post '/users/login' do
+      # Get user information from request body
+      data = JSON.parse(request.body.read)
+  
+      # Find user by email
+      user = User.find_by(email: data['email'])
+  
+      # Check if user exists and password matches
+      if user && user.authenticate(data['password'])
+        # Return success message with user details
+        { message: 'User authenticated successfully', user: user }.to_json
+      else
+        # Return error message
+        halt 401, { error: 'Invalid email or password' }.to_json
       end
+    end
         
-      # Close the database
-      # db.close
       
 end
